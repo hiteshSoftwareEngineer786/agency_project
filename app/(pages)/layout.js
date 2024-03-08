9ffeee9b21b1f46c0cbec27863b42055c33db855
chawlaHitesh <chawlahitesh236@gmail.com>
2024-03-08 22:53:47 +0530

    first commit

---
+++ app/(pages)/layout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import CommanLayout from '../comman/CommanLayout'
+
+const layout = ({children}) => {
+  return (
+    <CommanLayout>{children}</CommanLayout>
+  )
+}
+
+export default layout
